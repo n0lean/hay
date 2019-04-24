e8e3ec9d800a07d9c5468a84b44d1af26fda4e71
n0lean <cpy19960205@zju.edu.cn>
2019-04-24 00:34:48 -0400

    arn settings

---
+++ before_script.py
@@ -0,0 +1,11 @@
+import os
+import json
+
+
+if __name__ == '__main__':
+    with open('./.chalice/config.json', 'r') as f:
+        d = json.load(f)
+    d['stages']['dev']['manage_iam_role'] = False
+    d['stages']['dev']['iam_role_arn'] = os.environ['iam_role_arn']
+    with open('./chalice/config.json', 'w') as f:
+        json.dump(d, f)
