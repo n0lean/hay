935b2dcfef8e441dd0ebbe421a048869bba0d59b
n0lean <cpy19960205@zju.edu.cn>
2019-05-12 16:43:18 -0400

    .jpg

--- scripts/send_image_to_s3.py
+++ scripts/send_image_to_s3.py
@@ -1,4 +1,3 @@
-from chalicelib.model import Image, HayMsg
 import base64
 import time
 import requests
@@ -7,14 +6,8 @@ import requests
 def send_img(path):
     with open(path, 'rb') as f:
         b64_f = base64.b64encode(f.read()).decode('ascii')
-    haymsg = HayMsg()
-    img = Image('test_msg', 'imagetest', b64_f, 10, 10, str(time.time()))
-    haymsg.add_msg(img)
-    print(haymsg.to_json())
-    r = requests.post('https://79dcwe44n1.execute-api.us-east-1.amazonaws.com/api/img',
-                      data=haymsg.to_json())
-    print(r.json())
+    print(b64_f)
 
 
 if __name__ == '__main__':
-    send_img('../test/test_img.jpg')
+    send_img('../test/test_img2.jpg')
